0ac1c00d04df6a52d11ec260237210e36cf06018
joao_junio <joaojunio818@gmail.com>
2026-01-01 19:07:00 -0300

    Corrigindo rota da API

--- src/main/java/com/joaojuniodev/sendingEmail/controller/EmailController.java
+++ src/main/java/com/joaojuniodev/sendingEmail/controller/EmailController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/email")
+@RequestMapping("/api/email/v1")
 public class EmailController {
 
     @Autowired
